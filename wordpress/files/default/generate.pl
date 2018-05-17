a7b4d22397b5609cf115968fa6e9c32e7aea4f2a
Satish <xsatishx@gmail.com>
2018-05-17 14:59:54 +0800

    changes to generate sql creds file automatically

--- wordpress/files/default/generate.pl
+++ wordpress/files/default/generate.pl
@@ -9,7 +9,7 @@ while (<fh>)
 close fh or die $!;
 open(FH, '>', "/tmp/createdb.sh") or die $!;
 
-print FH "mysql -u root -p$lines[0] <<MYSQL_SCRIPT\n";
+print FH "mysql -u root -prootpassword <<MYSQL_SCRIPT\n";
 print FH "CREATE DATABASE $lines[1] DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;\n";
 print FH "GRANT ALL ON $lines[1].* TO \'$lines[2]\'@\'%\' IDENTIFIED BY \'$lines[3]\';\n";
 print FH "FLUSH PRIVILEGES; \n";
